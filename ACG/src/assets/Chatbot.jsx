d6ff312c6712973c8a55953ea88a3bc8a5599f58
anshhhhhhh <anshbansal19906@gmail.com>
2024-08-27 18:01:23 +0530

    API integration with gemini

--- ACG/src/assets/Chatbot.jsx
+++ ACG/src/assets/Chatbot.jsx
@@ -2,16 +2,41 @@ import React, { useState } from 'react';
 import Message from './Message';
 import InputArea from './InputArea';
 import './Chatbot.css';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 const Chatbot = () => {
+
   const [messages, setMessages] = useState([]);
 
-  const handleSendMessage = (message) => {
-    setMessages([...messages, { text: message, type: 'user' }]);
+  async function run(message) {
+    try {
+      const prompt = message
+
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
     
-    setTimeout(() => {
-      setMessages([...messages, { text: message, type: 'user' }, { text: `Echo: ${message}`, type: 'bot' }]);
-    }, 1000);
+
+      setMessages(prevMessages => [
+        ...prevMessages,
+        { text, type: 'bot' },
+      ]);
+    } catch (error) {
+      console.error(':( ::', error);
+    }
+  }
+
+  const handleSendMessage = (message) => {
+    setMessages(prevMessages => [
+      ...prevMessages,
+      { text: message, type: 'user' }
+    ]);
+
+    run(message);
   };
 
   return (
@@ -27,3 +52,4 @@ const Chatbot = () => {
 };
 
 export default Chatbot;
+
